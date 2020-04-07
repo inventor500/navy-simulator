632e34605ed7d43dcee1a62cfd103e261f16660e
inventor500 <isaacganoung@gmail.com>
2020-04-07 08:54:04 -0500

    Defined functions in Ship class.

--- Ship.cpp
+++ Ship.cpp
@@ -8,19 +8,30 @@ Ship::Ship(std::string name, std::string id, int maxSpeed)
 void Ship::deploy(ATime t, int x, int y, int heading, int speed) {
   // place the ship at (x, y) with given heading and speed
   // remember to keep track of the ships history of locations
-  
+  Location.x = x;
+  Location.y = y;
+  setVelocity(heading, speed);
+  updatePosition(t);
 }
 
 void Ship::changeOrders(int heading, int speed) {
-  // update ship directions
+  setVelocity(heading, speed);
 }
 
-void Ship::accept(const Order& order) {
+void Ship::accept(const Order& order) {p
   // For use later, keep commented
   //order.execute(*this);
 }
 
 void Ship::updatePosition(ATime t) {
   // update the ship's current position to the given ATime t
+  //keep track of history here
+  if (isDeployed()) {
+    position.x += velocity.x * (t - time);
+    position.y += velocity.y * (t - time);
+    Location location(t, position);
+    history.push_back(position);
+    time = t;
+  }
 }
 
