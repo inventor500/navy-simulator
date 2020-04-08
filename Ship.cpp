04038a3ca5d126409473ad66b14104d0530daae1
inventor500 <isaacganoung@gmail.com>
2020-04-07 21:57:12 -0500

    Fixed Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -8,17 +8,19 @@ Ship::Ship(std::string name, std::string id, int maxSpeed)
 void Ship::deploy(ATime t, int x, int y, int heading, int speed) {
   // place the ship at (x, y) with given heading and speed
   // remember to keep track of the ships history of locations
-  Location.x = x;
-  Location.y = y;
+  position.x = x;
+  position.y = y;
   setVelocity(heading, speed);
   updatePosition(t);
+  deployed = true;
+  time = t;
 }
 
 void Ship::changeOrders(int heading, int speed) {
   setVelocity(heading, speed);
 }
 
-void Ship::accept(const Order& order) {p
+void Ship::accept(const Order& order) {
   // For use later, keep commented
   //order.execute(*this);
 }
@@ -27,11 +29,10 @@ void Ship::updatePosition(ATime t) {
   // update the ship's current position to the given ATime t
   //keep track of history here
   if (isDeployed()) {
-    position.x += velocity.x * (t - time);
-    position.y += velocity.y * (t - time);
+    position += velocity * (t - time);
     Location location(t, position);
-    history.push_back(position);
-    time = t;
+    history.push_back(location);
   }
+  time = t;
 }
 
